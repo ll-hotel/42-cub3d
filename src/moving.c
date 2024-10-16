c75c48991d19969c6e9c8f81d5f0b90f4199390f
ll-hotel <150347935+ll-hotel@users.noreply.github.com>
2024-10-16 17:35:51 +0000

    Changed wall collision check. We can slide alongside walls! (#20)

--- src/moving.c
+++ src/moving.c
@@ -6,7 +6,7 @@
 /*   By: ll-hotel <ll-hotel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 01:38:28 by ll-hotel          #+#    #+#             */
-/*   Updated: 2024/09/08 20:36:21 by ll-hotel         ###   ########.fr       */
+/*   Updated: 2024/10/16 16:40:53 by ll-hotel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,11 @@
 #include "vec2f.h"
 #include <math.h>
 
-static void		turn_player(t_player *player);
-static void		walk_player(t_cube *cube, t_player *player);
-static void		strafe_player(t_cube *cube, t_player *player);
-static float	nearest_wall_distance(t_cube *cube, t_player *player);
+static void	turn_player(t_player *player);
+static void	walk_player(t_cube *cube, t_player *player);
+static void	strafe_player(t_cube *cube, t_player *player);
+
+static void	adjust_step(t_cube *cube, t_player *player, t_vec2f *step);
 
 void	move_player(t_cube *cube, t_player *player)
 {
@@ -51,45 +52,54 @@ void	turn_player(t_player *player)
 
 void	walk_player(t_cube *cube, t_player *player)
 {
-	t_vec2f const	dir = player->dir;
-	t_vec2f			step;
-	float			axis;
+	t_vec2f	step;
+	float	axis;
 
 	axis = player->axis + PI * (player->walking == -1);
 	if (axis > 2 * PI)
 		axis -= 2 * PI;
 	else if (axis < 0)
 		axis += 2 * PI;
-	player->dir = vec2f(cosf(axis), -sinf(axis));
-	step = vec2f_scal(player->dir, 0.1f);
-	if (nearest_wall_distance(cube, player) > 0.2)
-		player->pos = vec2f_add(step, player->pos);
-	player->dir = dir;
+	step = vec2f_scal(vec2f(cosf(axis), -sinf(axis)), 0.1f);
+	adjust_step(cube, player, &step);
+	player->pos = vec2f_add(step, player->pos);
 }
 
 void	strafe_player(t_cube *cube, t_player *player)
 {
-	t_vec2f const	dir = player->dir;
-	t_vec2f			step;
-	float			axis;
+	t_vec2f	step;
+	float	axis;
 
 	axis = player->axis + PI * 0.5 * player->strafing;
 	if (axis > 2 * PI)
 		axis -= 2 * PI;
 	else if (axis < 0)
 		axis += 2 * PI;
-	player->dir = vec2f(cosf(axis), -sinf(axis));
-	step = vec2f_scal(player->dir, 0.1f);
-	if (nearest_wall_distance(cube, player) > 0.2)
-		player->pos = vec2f_add(step, player->pos);
-	player->dir = dir;
+	step = vec2f_scal(vec2f(cosf(axis), -sinf(axis)), 0.1f);
+	adjust_step(cube, player, &step);
+	player->pos = vec2f_add(step, player->pos);
 }
 
-static float	nearest_wall_distance(t_cube *cube, t_player *player)
+static void	adjust_step(t_cube *cube, t_player *player, t_vec2f *step)
 {
-	t_ray	ray;
+	long const	y = floorf(player->pos.y + step->y);
+	long const	x = floorf(player->pos.x + step->x);
+	float		dist[4];
 
-	ray_init_dda(&ray, player, SCREEN_WIDTH / 2);
-	ray_perform_dda(&ray, cube);
-	return (ray.perpwalldist);
+	dist[0] = player->pos.y + step->y - y;
+	dist[1] = 1.f - dist[0];
+	dist[2] = player->pos.x + step->x - x;
+	dist[3] = 1.f - dist[2];
+	if (y < 1 || y + 1 >= cube->map.height)
+		step->y = 0;
+	else if (dist[0] < 0.2f && cube->map.cells[y - 1][x] == WALL)
+		step->y += 0.2f - dist[0];
+	else if (dist[1] < 0.2f && cube->map.cells[y + 1][x] == WALL)
+		step->y -= 0.2f - dist[1];
+	if (x < 1 || x + 1 >= cube->map.width)
+		step->x = 0;
+	else if (dist[2] < 0.2f && cube->map.cells[y][x - 1] == WALL)
+		step->x += 0.2f - dist[2];
+	else if (dist[3] < 0.2f && cube->map.cells[y][x + 1] == WALL)
+		step->x -= 0.2f - dist[3];
 }
